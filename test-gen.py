bc40c267e99e5235434b6d4bfb968c40ab7596cb
Simon Woo <simonwoo@usc.edu>
2016-09-05 17:29:10 +0100

    update

--- test-gen.py
+++ test-gen.py
@@ -10,6 +10,9 @@ import urllib2
 from pprint import pprint
 import xml.etree.ElementTree as ET
 
+#import nltk.parse.dependencygraph
+from nltk.parse import DependencyGraph
+
 
 def inputsentence_analysis(inputsentence):
 	post_data = "sentence="+inputsentence
@@ -21,18 +24,30 @@ def inputsentence_analysis(inputsentence):
         #if returncode != 200:
          #       print >> log,'NLP server error (problem processing)'
 
-        print content
+        #print content
+	#print type(content)
+
+
+
         content=content.split('\n')
-        sent=[]
-        result={}
-        SemPar = 0
-        NoSbj = 0
+        sent=""
+        #result={}
+        #SemPar = 0
+        #NoSbj = 0
         #print "data is", repr(content)
+	#print content	
 
         for row in content:
                 table=row.split('\t')
-                sent.append(table)
-
+                #sent.append([table[0], table[1], table[2], table[4],  table[5], table[6], table[8], table[10], table[12], table[13], "\n"])
+                sent+=table[0]+"\t"+table[1]+"\t"+table[2]+"\t"+table[4]+"\t"+table[5]+"\t"+table[6]+"\t"+table[8]+"\t"+table[10]+"\t"+table[12]+"\t"+table[13]+"\n"
+		#sent+=table[0]+"\t"+table[1]+"\n"
+	print sent
+	dg = DependencyGraph(sent)
+	tree = dg.tree()
+	print tree.pprint()
+	#print(dg)
+	print(dg.to_conll(4))
 
 def demo():
         inputsentences = "Install the spacer between the two washers."
