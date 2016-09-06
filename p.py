333c35a165612cebb429f30af1fe19effc9bc9c0
cdimarcoUW <cdimarco@uwaterloo.ca>
2016-09-06 11:59:57 +0100

    Create p.py

---
+++ p.py
@@ -0,0 +1,34 @@
+lexicon_p = 
+[('pack', 'v'), 
+('paint', 'tn'), # paints, painted
+('pair', 'n'),
+('parallel', 'adj'),
+('park', 'v'), # parks, parked, parked, parking(adj)
+('part', 'n')
+('particle', 'n'),
+('particular', 'adj'), 
+('paste', 'n'),
+('patch', 'n'),
+('performance', 'n'),
+('period', 'n'),
+('permanent', 'adj'),
+('permanently', 'adv'),
+('permitted', 'adj'),
+('person', 'n'),
+('personnel', 'n'),
+('piece', "n'),
+('play','n'),
+('plus', 'pre'),
+('pneumatic', 'adj'),
+('pneumatically', 'adv'),
+('point', 'n'),
+('point', 'v'), # points, pointed, pointed
+('poisonous', 'adj'),
+('polish', 'v'), # polishes, polished, polished
+('position', 'n'),
+('possible','adj'),
+('possibly', 'adv'),
+('post-', 'prefix'),
+('pre-', 'prefix'),
+('precaution', 'n'),
+]
