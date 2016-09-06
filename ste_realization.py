3f97da999f7797fb23fb891e16fc0fb3a805dde3
Dave Howcroft <dave.howcroft@gmail.com>
2016-09-06 12:16:01 +0100

    added specialized handling of NPs from Pierre-Luc (plvaudry) and Rodrigo (rdeoliveira)

--- ste_realization.py
+++ ste_realization.py
@@ -13,27 +13,30 @@ def realize(deptree, with_stemming=True):
     :type deptree: DependencyTree
     :return:
     """
-    if with_stemming:
-        out_string = stemmer.stem(deptree.label())
+    if deptree.features.get("dependency_label") in ("nsubj", "dobj", "conj", "compound"):
+        return realize_noun_phrase(deptree, with_stemming)
     else:
-        out_string = deptree.label()
-    left_edge_buffer = []
-    left_mid_buffer = []
-    left_buffer = []
-    for child in deptree:
-        attachment_decision = where_to_attach(child)
-        if attachment_decision == -3:
-            left_edge_buffer.append(realize(child, with_stemming))
-        elif attachment_decision == -2:
-            left_mid_buffer.append(realize(child, with_stemming))
-        elif attachment_decision == -1:
-            left_buffer.append(realize(child, with_stemming))
-        elif attachment_decision == 1:
-            out_string = out_string + " " + realize(child, with_stemming)
-    for buffer in left_buffer, left_mid_buffer, left_edge_buffer:
-        if buffer:
-            out_string = " ".join(buffer) + " " + out_string
-    return out_string
+        if with_stemming:
+            out_string = stemmer.stem(deptree.label())
+        else:
+            out_string = deptree.label()
+        left_edge_buffer = []
+        left_mid_buffer = []
+        left_buffer = []
+        for child in deptree:
+            attachment_decision = where_to_attach(child)
+            if attachment_decision == -3:
+                left_edge_buffer.append(realize(child, with_stemming))
+            elif attachment_decision == -2:
+                left_mid_buffer.append(realize(child, with_stemming))
+            elif attachment_decision == -1:
+                left_buffer.append(realize(child, with_stemming))
+            elif attachment_decision == 1:
+                out_string = out_string + " " + realize(child, with_stemming)
+        for buffer in left_buffer, left_mid_buffer, left_edge_buffer:
+            if buffer:
+                out_string = " ".join(buffer) + " " + out_string
+        return out_string
 
 
 def where_to_attach(deptree):
@@ -54,6 +57,43 @@ def where_to_attach(deptree):
         return 1
 
 
+def realize_noun_phrase(deptree, with_stemming=True):
+    if with_stemming:
+        noun = stemmer.stem(deptree.label())
+    else:
+        noun = deptree.label()
+    out_string = noun
+    det = []
+    amod = []
+    compound = []
+    prep = []
+    cc = []
+    conj = []
+    for child in deptree:
+        dep = child.features.get("dependency_label")
+        if dep == "det":
+            det.append(realize(child, with_stemming))
+        elif dep in ("amod", "nummod"):
+            amod.append(realize(child, with_stemming))
+        elif dep in ("compound", ):
+            compound.append(realize(child, with_stemming))
+        elif dep in ("prep", ):
+            prep.append(realize(child, with_stemming))
+        elif dep == "cc":
+            cc.append(realize(child, with_stemming))
+        elif dep == "conj":
+            conj.append(realize(child, with_stemming))
+        else:
+            amod.append(realize(child, with_stemming))
+    for buffer in det, amod, compound:
+        if buffer:
+            out_string = " ".join(buffer) + " " + out_string
+    for buffer in prep, cc, conj:
+        if buffer:
+            out_string = out_string + " " + " ".join(buffer)
+    return out_string
+        
+
 if __name__ == "__main__":
     try:
         with open("ste100.pickle", 'rb') as ste_pickle:
@@ -65,3 +105,8 @@ if __name__ == "__main__":
     print(examples[0][1])
     print(realize(examples[0][1]))
     print(realize(examples[0][1], with_stemming=False))
+    for example in examples:
+        print(example[0])
+        print(example[1])
+        print(realize(example[1], with_stemming=False))
+        print()
